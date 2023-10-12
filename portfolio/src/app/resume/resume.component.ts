90c3be29ceba747ed994c90c4e2efced4b487744
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-12 19:53:19 +0530

    date-12-10-2023

--- portfolio/src/app/resume/resume.component.ts
+++ portfolio/src/app/resume/resume.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent {
-
+  viewResume() {
+    // Replace 'path/to/your/resume.pdf' with the actual path or URL to your resume PDF file
+    const resumeUrl = '../../assets/Resume.pdf';
+    
+    window.open(resumeUrl, '_blank');
+  }
 }
