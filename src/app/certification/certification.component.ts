ce587f44c01bfeb738b0d281bec3f3055e4ed498
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-16 15:58:20 +0530

    16-10-2023

--- src/app/certification/certification.component.ts
+++ src/app/certification/certification.component.ts
@@ -5,21 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './certification.component.html',
   styleUrls: ['./certification.component.css']
 })
-export class CertificationComponent {
+export class CertificationComponent implements OnInit {
   slideIndex = 1;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.showSlides(this.slideIndex);
+  }
 
   plusSlides(n: number) {
     this.showSlides(this.slideIndex += n);
   }
 
+  currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
+  }
+
   showSlides(n: number) {
     let i;
-    const slides = document.getElementsByClassName('mySlides') as HTMLCollectionOf<HTMLElement>;
+    let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) { this.slideIndex = 1; }
     if (n < 1) { this.slideIndex = slides.length; }
     for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
+      slides[i].style.display = "none";
     }
-    slides[this.slideIndex - 1].style.display = 'block';
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex - 1].style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
   }
 }
