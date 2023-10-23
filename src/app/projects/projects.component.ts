75781659a9718234dc251a296c54d4aa4d92d4ae
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-23 14:54:18 +0530

    Modified

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,33 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-  customOptions: any = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: false,
-    navSpeed: 700,
-    navText: ['<', '>'],
-    responsive: {
-      0: { items: 1 },
-      400: { items: 2 },
-      740: { items: 3 },
-      940: { items: 4 }
+  viewOtherProj() {
+    const resumeUrl = 'https://github.com/Jayasri2021?tab=repositories';
+      window.open(resumeUrl, '_blank');
     }
-  };
-
-  projects = [
-    {
-      name: 'Project 1',
-      description: 'This is the first project',
-      image: 'path-to-image-1.jpg'
-    },
-    {
-      name: 'Project 2',
-      description: 'This is the second project',
-      image: 'path-to-image-2.jpg'
-    },
-    // Add more projects as needed
-  ];
+  
 }
