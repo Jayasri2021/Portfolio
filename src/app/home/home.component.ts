424544aca99f502c8d931e8e72651ca4caf9ccc7
Jayasri <jayasri.suresh2021@gmail.com>
2023-12-23 22:39:28 +0530

    resume

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit{
   viewResume() {
-  const resumeUrl = '../../assets/resume.pdf';
+  const resumeUrl = '../../assets/Resume.pdf';
     window.open(resumeUrl, '_blank');
   }
   constructor(private router: Router) { }
